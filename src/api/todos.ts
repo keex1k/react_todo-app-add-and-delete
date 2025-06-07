f993f7151ef25c11ce252fecf0b90574c084d526
Bartek <kowalski_1410@wp.pl>
2025-06-07 23:00:44 +0200

    initial commit

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,10 +1,14 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+export const USER_ID = 3032;
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
+};
+
+export const addTodos = (todo: Todo) => {
+  return client.post<Todo[]>(`/todos?userId=${USER_ID}`, todo);
 };
 
 // Add more methods here
